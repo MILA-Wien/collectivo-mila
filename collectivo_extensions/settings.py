e5205e5501102fbf99027f0e0b77c689c5e42c59
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2022-12-01 13:09:10 +0100

    Adjust settings

--- collectivo_extensions/settings.py
+++ collectivo_extensions/settings.py
@@ -2,6 +2,11 @@
 from collectivo_app.settings import *
 
 
-# INSTALLED_APPS += [
-#     'collectivo_extensions.mila_members'
-# ]
+INSTALLED_APPS += [
+    'collectivo_extensions.mila_members'
+]
+
+# TODO Find better solution
+# Remove devtools because populate command uses
+# the built-in members extension
+INSTALLED_APPS.remove('collectivo.devtools')
